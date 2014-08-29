51c06be49b6855a373da6062de175ff63049022e
Mikhail Mutcianko <mutcianko.m@gmail.com>
2014-08-29 20:51:44 +0400

    add android completion tests
    - add android plugin test classes as test library
    - add base android test cases for kotlin, setting up android facet and sdk

--- generators/src/org/jetbrains/jet/generators/tests/GenerateTests.kt
+++ generators/src/org/jetbrains/jet/generators/tests/GenerateTests.kt
@@ -138,6 +138,7 @@ import org.jetbrains.k2js.test.semantics.AbstractDynamicTest
 import org.jetbrains.jet.lang.resolve.android.AbstractAndroidXml2KConversionTest
 import org.jetbrains.jet.android.AbstractCrossParserTest
 import org.jetbrains.jet.lang.resolve.android.AbstractAndroidBoxTest
+import org.jetbrains.jet.android.AbstractAndroidCompletionTest
 
 fun main(args: Array<String>) {
     System.setProperty("java.awt.headless", "true")
@@ -661,6 +662,10 @@ fun main(args: Array<String>) {
         
         testClass(javaClass<AbstractCrossParserTest>()) {
             model("android/crossParser", recursive = false, extension = null)
+        }
+
+        testClass(javaClass<AbstractAndroidCompletionTest>()) {
+            model("android/completion", recursive = false, extension = null)
         }
     }
 
